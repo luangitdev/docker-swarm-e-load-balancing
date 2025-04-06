0715a9ef4018abc956250b8ef8e2f769afc13a5c
luangitdev <luaninveste@gmail.com>
2025-04-06 14:04:18 -0300

    feat: adicionar variavel container_id que captura o ID do container e apresenta no print da tela para validar o balancemento ao acessar a aplicação

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask # type: ignore
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a Flask application app running in Docker Swarm.'
+    # Obtém o ID curto do container
+    container_id = os.getenv('HOSTNAME', 'unknown')
+    return f'Hello, World! This is replica: {container_id}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
